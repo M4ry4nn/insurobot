924d242087905d46fe9e41e36d1680d356b8530c
Adrian Krebs <krebs-adrian@bluewin.ch>
2016-09-17 19:25:50 +0200

    ewr

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ function receivedMessage(event) {
                 body: JSON.stringify(bodyObject)},
             function (error, response, body) {
                 console.log("--------------------------------------------------------"+body);
-                sendTextMessage(senderID, body[0]);
+                sendTextMessage(senderID, body);
 
                 var obj = {"REPORT-CLAIM-IMAGE": body[0]};
                 console.log("-------------------------------------------------------OBJ"+obj);
